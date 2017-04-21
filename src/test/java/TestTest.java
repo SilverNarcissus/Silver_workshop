d88da6e861fd711b9117ca5ab2de3eff3a591ebc
151250176 <151250176@smail.nju.edu.cn>
2017-04-21 18:43:20 +0800

    add junit

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by SilverNarcissus on 2017/4/21.
+ */
+public class TestTest {
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+}
