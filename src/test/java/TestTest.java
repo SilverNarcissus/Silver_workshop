0fe18f0c0e5030030925905210ea86ae8dc073dd
151250176 <151250176@smail.nju.edu.cn>
2017-04-21 20:54:26 +0800

    add a junit test

--- src/test/java/TestTest.java
+++ src/test/java/TestTest.java
@@ -6,7 +6,10 @@ import static org.junit.Assert.*;
 public class TestTest {
     @org.junit.Test
     public void main() throws Exception {
-
+        double sum = 0;
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += Math.sqrt(i);
+        }
+        System.out.println(sum);
     }
-
 }
